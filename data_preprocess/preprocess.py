ad25124aea447a731225b5023ef2383c7eaad9f7
Aris Markogiannakis <81876062+arismarkogi@users.noreply.github.com>
2025-03-24 01:12:46 +0200

    Update preprocess.py

--- data_preprocess/preprocess.py
+++ data_preprocess/preprocess.py
@@ -9,22 +9,17 @@ if len(sys.argv) != 3:
 input_adata_path = sys.argv[1]
 output_dataset_path = sys.argv[2]
 
-try:
-    data = sc.read_h5ad(input_adata_path)
-    data.obs['n_counts'] = data.X.sum(axis=1)
-    data.var['ensembl_id'] = data.var['feature_id']
 
-    tk = LangCellTranscriptomeTokenizer(dict([(k, k) for k in data.obs.keys()]), nproc=4)
-    tokenized_cells, cell_metadata = tk.tokenize_anndata(data)
-    tokenized_dataset = tk.create_dataset(tokenized_cells, cell_metadata)
+data = sc.read_h5ad(input_adata_path)
+data.obs['n_counts'] = data.X.sum(axis=1)
+data.var['ensembl_id'] = data.var['feature_id']
 
-    tokenized_dataset.save_to_disk(output_dataset_path)
+tk = LangCellTranscriptomeTokenizer(dict([(k, k) for k in data.obs.keys()]), nproc=4)
+tokenized_cells, cell_metadata = tk.tokenize_anndata(data)
+tokenized_dataset = tk.create_dataset(tokenized_cells, cell_metadata)
 
-    print(f"Tokenized dataset saved to: {output_dataset_path}")
+tokenized_dataset.save_to_disk(output_dataset_path)
 
-except FileNotFoundError:
-    print(f"Error: Input file not found: {input_adata_path}")
-    sys.exit(1)
-except Exception as e:
-    print(f"An error occurred: {e}")
-    sys.exit(1)
+print(f"Tokenized dataset saved to: {output_dataset_path}")
+
+
